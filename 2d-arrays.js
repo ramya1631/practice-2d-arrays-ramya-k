4f84763b9977746f5cad24854b492a8730e05964
ramya <ramya.koluguri16@gmail.com>
2025-03-19 23:42:45 -0500

    2d-array

---
+++ 2d-arrays.js
@@ -0,0 +1,27 @@
+//Task 1 - Student Scores
+
+let allScores = [
+    [35,36,37],
+    [45,46,47],
+    [55,56,57]
+];
+
+console.log(allScores);
+
+//Task 2 - Build a Seating Grid
+let theaterSeating = [
+    ["C1","C2","C3","C4"],
+    ["B1","B2","B3","B4"],
+    ["A1","A2","A3","A4"]
+];
+
+console.log(theaterSeating);
+
+//Task 3 - Form a Multi-Dimensional Contact List
+
+let contactsList = [
+    ["Tim","tim@gmail.com","1800-949-2440"],
+    ["Rob","rob@gmail.com","1800-949-2441"]
+];
+
+console.log(contactsList);
